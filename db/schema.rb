44a353e1c3e73e595c8089cdb9a5eb4788e9b549
lbackman <lucasbackman@gmail.com>
2023-07-09 08:57:25 +0300

    Add Notification model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_055048) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_055405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_055048) do
     t.index "GREATEST(sender_id, receiver_id), LEAST(sender_id, receiver_id)", name: "index_friend_requests_on_interchangable_sender_id_and_receiver_", unique: true
     t.index ["receiver_id"], name: "index_friend_requests_on_receiver_id"
     t.index ["sender_id"], name: "index_friend_requests_on_sender_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
   create_table "user_informations", force: :cascade do |t|
---
+++ spec/factories/notifications.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :notification do
+    recipient { nil }
+    type { "" }
+    params { "" }
+    read_at { "2023-07-09 08:54:05" }
+  end
+end
