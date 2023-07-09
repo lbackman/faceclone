72778ac622c4157dae6599ea8739fd45c74e1006
lbackman <lucasbackman@gmail.com>
2023-07-09 11:59:32 +0300

    Copy routes from old project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # creating routes for user posts
+  # https://stackoverflow.com/questions/31757006/rails-4-how-do-i-add-an-index-route-for-a-nested-resource-in-order-to-list-al
+  
+  resources :users, except: [:new, :create] do
+    resources :friend_requests, only: :index
+    resources :posts, except: :index
+    resource :user_information, only: [:edit, :update]
+  end
+
+  resources :posts, only: :index do
+    resources :comments, module: :posts
+  end
+
+  resources :friend_requests, only: [:create, :update, :destroy]
+  get 'users/:id/friends', to: 'users#friends', as: :friends
+
+  resources :likes, only: [:create, :destroy]
+
   # Defines the root path route ("/")
-  root "users#index"
+  authenticated :user do
+    root to: "posts#index"
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new", as: :unauthenticated_root
+  end
 end
