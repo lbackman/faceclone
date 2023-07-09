156a612102945e22f5aa188e79ba548007bbd8dd
lbackman <lucasbackman@gmail.com>
2023-07-09 10:54:20 +0300

    Copy notifications from old project

--- app/notifications/friend_request_notification.rb
+++ app/notifications/friend_request_notification.rb
@@ -1,27 +1,28 @@
 # To deliver this notification:
 #
-# FriendRequestNotification.with(post: @post).deliver_later(current_user)
+# FriendRequestNotification.with(friend_request: @friend_request).deliver_later(friend_request.receiver)
 # FriendRequestNotification.with(post: @post).deliver(current_user)
 
 class FriendRequestNotification < Noticed::Base
   # Add your delivery methods
   #
-  # deliver_by :database
+  deliver_by :database
+  # deliver_by :action_cable
   # deliver_by :email, mailer: "UserMailer"
   # deliver_by :slack
   # deliver_by :custom, class: "MyDeliveryMethod"
 
   # Add required params
   #
-  # param :post
+  param :friend_request
 
   # Define helper methods to make rendering easier.
   #
-  # def message
-  #   t(".message")
-  # end
+  def message
+    t(".message", sender: User.find(params[:friend_request][:sender_id]).first_name)
+  end
   #
-  # def url
-  #   post_path(params[:post])
-  # end
+  def url
+    user_path(User.find(params[:friend_request][:sender_id]))
+  end
 end
