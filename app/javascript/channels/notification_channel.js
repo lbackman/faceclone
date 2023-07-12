39783bf5e614a9162dd47574b37c0ecc015f0140
lbackman <lucasbackman@gmail.com>
2023-07-12 15:00:50 +0300

    Increment notification counter action cable

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -10,7 +10,17 @@ consumer.subscriptions.create("Noticed::NotificationChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
     console.log(data)
+    this.incrementNotificationCount()
+  },
+
+  incrementNotificationCount() {
+    const notificationCount = document.querySelector('#notification-count')
+    const currentCount = parseInt(notificationCount.innerText)
+    if (currentCount < 9) {
+      notificationCount.innerText = currentCount + 1
+    } else {
+      notificationCount.innerText = "9+"
+    }
   }
 });
