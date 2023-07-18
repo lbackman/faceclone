4640c47068454bbed9ed3c94cc00277c44c2b0da
lbackman <lucasbackman@gmail.com>
2023-07-18 16:32:31 +0300

    Make shared modal for post and comment likes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def likes_index_path(likeable:, parent:)
+    if parent
+      comment_likes_path(parent, likeable)
+    else
+      post_likes_path(likeable)
+    end
+  end
 end
