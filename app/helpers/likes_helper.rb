1158e7c235f1578b36ce71435dea6054ff0dbc5a
lbackman <lucasbackman@gmail.com>
2023-07-19 15:50:00 +0300

    Change button to link

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -6,4 +6,12 @@ module LikesHelper
       post_likes_path(likeable)
     end
   end
+
+  def like_sentence_helper(number_of_likes)
+    if number_of_likes == 1
+      "1 person likes this"
+    else
+      "#{number_of_likes} people like this"
+    end
+  end
 end
