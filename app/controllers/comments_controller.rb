c9e3ef0a91d14372548b2bb8e3d255badbbb6486
lbackman <lucasbackman@gmail.com>
2023-08-12 11:13:52 +0300

    Use realtime update when comment is posted
    Implemented with turbostream.
    Can not render the like button with the comment however because
    it uses the curren_user Devise helper method, which isn't available
    with turbo streams:
    Will try to fix with the following:
    https://stackoverflow.com/questions/68730001/use-current-user-in-hotwire-partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,17 @@ class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.new(comment_params)
     @comment.author = current_user
-    @comment.save
-    redirect_back(fallback_location: @commentable)
+    
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('comment_form', partial: 'comments/form', locals: { commentable: @commentable, comment: Comment.new }) }
+        format.html { render partial: 'comments/form', locals: { commentable: @commentable, comment: Comment.new }}
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('comment_form', partial: 'comments/form', locals: { commentable: @commentable, comment: @comment }) }
+        format.html { render partial: 'comments/form', locals: { commentable: @commentable, comment: @comment }}
+      end
+    # redirect_back(fallback_location: @commentable)
+    end
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,16 @@
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :author, class_name: 'User'
   belongs_to :commentable, polymorphic: true, optional: true, counter_cache: true
   has_many :likes, as: :likeable, dependent: :destroy
 
   validates :body, presence: true
+
+  after_create_commit {
+    broadcast_append_to [commentable, :comments],
+      target: "#{dom_id(commentable, :comments)}",
+      partial: "comments/comment",
+      locals: { comment: self, commentable: }
+  }
 end
