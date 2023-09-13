3c99aef8c1860f6d558d6a31ec3fa62886b2cd6f
lbackman <lucasbackman@gmail.com>
2023-09-13 16:20:41 +0300

    Show friend count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    @friend_amount = User.friends(@user).size
     @friend_request = FriendRequest.mutual(current_user, @user) || FriendRequest.new
     if current_user == @user || current_user.friends_with?(@user)
       @posts = Post.where(author: @user).with_author_information.order(created_at: :desc).page(params[:page])
