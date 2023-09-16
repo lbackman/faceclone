da72fbd976014a1edcffdd593136f66029800534
lbackman <lucasbackman@gmail.com>
2023-09-16 17:37:12 +0300

    Set up mailer for production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def welcome_email(user)
     @user = user
-    @url  = 'http://localhost:3000/users/sign_in'
+    @url  = new_user_session_url
     mail(to: @user.email, subject: 'Welcome to Odin Social Media App')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,22 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'osma.fly.dev' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  if Rails.application.credentials.email
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => Rails.application.credentials.email[:user_name],
+      :password             => Rails.application.credentials.email[:password],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }
+  end
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
